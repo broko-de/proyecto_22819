a7f5c8a99f89a2d1839eedaaf10d21fdb3fa91a9
Fedex <jose.liquin@bue.edu.ar>
2022-09-22 11:26:06 -0300

    Se actualiza ejemplos de urls

--- cac/urls.py
+++ cac/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('hola_mundo',views.hola_mundo),
+    path('saludar/',views.saludar,name="saludar_por_defecto"),
+    path('saludar/<str:nombre>',views.saludar,name="saludar"),
+    path('proyectos/<int:anio>/<int:mes>',views.ver_proyectos,name="ver_proyectos"),
+
 ]
--- cac/views.py
+++ cac/views.py
@@ -6,3 +6,15 @@ from django.shortcuts import render
 # Create your views here.
 def hola_mundo(request):
     return HttpResponse('Hola Mundo Django')
+
+def saludar(request,nombre='Pepe'):
+    return HttpResponse(f"""
+        <h1>Hola Mundo Django - {nombre}</h1>
+        <p>Estoy haciendo mi primera prueba</p>
+    """)
+
+def ver_proyectos(request,anio,mes):
+    return HttpResponse(f"""
+        <h1>Proyectos del  - {mes}/{anio}</h1>
+        <p>Listado de proyectos</p>
+    """)
