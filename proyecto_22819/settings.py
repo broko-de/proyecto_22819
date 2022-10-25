7a5f24c600edca8cb6947dd1641ae94aa04f124b
Fedex <jose.liquin@bue.edu.ar>
2022-10-25 11:29:03 -0300

    Clase 19: Django Base de datos

--- proyecto_22819/settings.py
+++ proyecto_22819/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proyecto_22819.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cac_22819', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Cac2022',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
