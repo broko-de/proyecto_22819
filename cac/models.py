4dd1be4572ed59e38886f4387b700e40683243cf
Fedex <jose.liquin@bue.edu.ar>
2022-11-01 11:51:59 -0300

    Clase 21: se agregan modelos de personas y categorias

--- cac/models.py
+++ cac/models.py
@@ -1,3 +1,16 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100,verbose_name='Nombre')
+    apellido = models.CharField(max_length=150,verbose_name='Apellido')
+    email = models.EmailField(max_length=150,null=True)
+    dni = models.IntegerField(verbose_name="DNI")
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=50,verbose_name='Nombre')
+    baja = models.BooleanField(default=0)
+
+    def __str__(self):
+        return self.nombre
